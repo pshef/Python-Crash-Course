2afabc201b9da38269054e221074dd29043d8f28
Pj Sheffield <78715072+pshef@users.noreply.github.com>
2021-09-12 17:29:53 -0500

    Exercise updates

--- 4-1_pizzas.py
+++ 4-1_pizzas.py
@@ -1,6 +1,23 @@
-pizza_types = ['pepperoni pizza', 'pineapple pizza', 'sausage pizza', 'spicy-as-all-get-out pizza']
+pizza_types = ['pepperoni pizza','pineapple pizza','sausage pizza','spicy-as-all-get-out pizza','dessert pizza']
 for pizza in pizza_types:
-    print(f"{pizza.capitalize()} is amazing and is real pizza, unlike the other types of pizza.")
+    print(f"{pizza.capitalize()} is amazing and is real pizza, unlike other types of pizza.")
     print("If you want to be my friend, you must eat this pizza.\n")
 
-print("Pizza is pretty great. And honestly, if you like any pizza we can still be friends. Even if you don't like real pizza.")
+print("Pizza is pretty great. And honestly, if you like any pizza we can still be friends. Even if you don't like real pizza.\n")
+
+print(f"The first three items in the list are: {pizza_types[:3]}\n")
+print(f"The middle three items in the list are: {pizza_types[1:4]}\n")
+print(f"And finally, the last three items in the list are: {pizza_types[-3:]}\n")
+
+friend_pizzas = pizza_types[:]
+
+pizza_types.append('cheese pizza')
+friend_pizzas.append('supreme pizza')
+
+print("My favorite pizzas are:")
+for pizza in pizza_types:
+    print(pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
---
+++ redone_pets.py
@@ -0,0 +1,6 @@
+pets = ['pet1', 'pet2', 'pet3', 'pet4']
+for animal in pets:
+    print(f"{animal.title()} is a good pet.")
+    print(f"I'm going to pet the hell out of you, {animal.title()}. \n")
+
+print(f"But really, {pets[3]} is the best.")
